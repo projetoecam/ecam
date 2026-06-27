6cceca257e7f01ee6182f2ef6689751913fcc8bd
Fornelos <thiagofornelos@gmail.com>
2026-06-27 17:40:07 -0300

    ajutes branch fornelos

--- src/main/java/com/ecam/ecam/Cadastros/repository/ComunidadeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecam.ecam.Cadastros.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public class ComunidadeRepository {
-
-}
