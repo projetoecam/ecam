c1ee807cfb5828a11a8c7ee00b24019f7f582406
Diego Borba <projetoecam@gmail.com>
2026-06-27 19:32:48 -0300

    Iniciando login com autenticacao jwt

---
+++ src/main/java/com/ecam/ecam/login/model/Usuario.java
@@ -0,0 +1,87 @@
+package com.ecam.ecam.login.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String login_usuario;
+    private String senha_hash;
+    private String perfil;
+    private boolean ativo;
+    private Date data_criacao;
+    private String codigo_sessao;
+    
+
+    public Usuario() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin_usuario() {
+        return login_usuario;
+    }
+
+    public void setLogin_usuario(String login_usuario) {
+        this.login_usuario = login_usuario;
+    }
+
+    public String getSenha_hash() {
+        return senha_hash;
+    }
+
+    public void setSenha_hash(String senha_hash) {
+        this.senha_hash = senha_hash;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public Date getData_criacao() {
+        return data_criacao;
+    }
+
+    public void setData_criacao(Date data_criacao) {
+        this.data_criacao = data_criacao;
+    }
+
+    public String getCodigo_sessao() {
+        return codigo_sessao;
+    }
+
+    public void setCodigo_sessao(String codigo_sessao) {
+        this.codigo_sessao = codigo_sessao;
+    }
+    
+}
