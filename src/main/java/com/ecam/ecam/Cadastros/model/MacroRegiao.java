a653964ca56f5f3010be89d5109e4e5acabb40d1
Fornelos <thiagofornelos@gmail.com>
2026-06-30 09:32:31 -0300

    Fornelos-MacroRegiao

---
+++ src/main/java/com/ecam/ecam/Cadastros/model/MacroRegiao.java
@@ -0,0 +1,34 @@
+package com.ecam.ecam.Cadastros.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_macro_regiao")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class MacroRegiao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_municipio", nullable = false)
+    private MacroRegiao macroRegiao;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 100)
+    private String regiao_apelido;
+
+
+
+    
+}
