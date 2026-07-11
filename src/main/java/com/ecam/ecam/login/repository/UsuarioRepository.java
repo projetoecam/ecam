5ba45532997dafa97a41e166faf3e4bc92d0ee76
Fornelos <thiagofornelos@gmail.com>
2026-07-11 16:43:53 -0300

    Ajuste de warn no usuario repository

--- src/main/java/com/ecam/ecam/login/repository/UsuarioRepository.java
+++ src/main/java/com/ecam/ecam/login/repository/UsuarioRepository.java
@@ -1,14 +1,13 @@
 package com.ecam.ecam.login.repository;
 
 import com.ecam.ecam.login.model.Usuario;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.Optional;
-
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     
     // Método para buscar o usuário no banco através do campo login_usuario.
